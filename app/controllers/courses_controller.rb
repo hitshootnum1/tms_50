eeb207734d55b6b611d7d4a9f48369410cd52c9c
Quang Vinh <quangvinh181193@gmail.com>
2016-04-07 13:42:56 +0700

    Trainee course subject

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,5 @@
+class CoursesController < ApplicationController
+  def show
+    @course = Course.find_by id: params[:id]
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create! name: "Vinh", email: "vinh@gmail.com", admin: true,
+  password: "123456", password_confirmation: "123456"
+5.times do |n|
+  Course.create! name: "Training-#{n+1}", description: "Traning course"
+end
+3.times do |n|
+  UserCourse.create! user_id: 1, course_id: n+1, status: false
+end
+5.times do |n|
+  Subject.create! name: "Subject-#{n+1}"
+end
+5.times do |n|
+  CourseSubject.create! course_id: 1, subject_id: n+1
+end
