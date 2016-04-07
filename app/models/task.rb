88a54a6c5e3b846ecd0d63e7adb08c60c12ac8ee
HUSTToanLH <laihuutoan@gmail.com>
2016-04-07 15:56:28 +0700

    subject show

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ActiveRecord::Base
   has_many :course_subject_tasks, dependent: :destroy
-  has_many :course_subject, through: :course_subject_tasks, dependent: :destroy
+  has_many :course_subjects, through: :course_subject_tasks, dependent: :destroy
 
   belongs_to :subject
 end
--- db/migrate/20160406101946_create_tasks.rb
+++ db/migrate/20160406101946_create_tasks.rb
@@ -2,6 +2,7 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.string :name
+      t.text :content
       t.boolean :modify
       t.references :subject, index: true, foreign_key: true
 
