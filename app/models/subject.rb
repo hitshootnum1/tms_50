39d2b0446f83264e7388cde664d8a96e85e0fd80
Quang Vinh <quangvinh181193@gmail.com>
2016-04-08 14:30:38 +0700

    Update & delete subject

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -4,7 +4,7 @@ class Subject < ActiveRecord::Base
   has_many :courses, through: :course_subjects, dependent: :destroy
 
   accepts_nested_attributes_for :tasks,
-    reject_if: proc {|attributes| attributes[:description].blank?},
+    reject_if: proc {|attributes| attributes[:name].blank?},
     allow_destroy: true
 
   validates :name, presence: true
