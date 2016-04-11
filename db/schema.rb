ce4e2fbea6baa6d6166dc7f808d5e00d75154f3f
Quang Vinh <quangvinh181193@gmail.com>
2016-04-11 10:19:47 +0700

    Finish install Sidekiq, Whenever
    d

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160410155443) do
+ActiveRecord::Schema.define(version: 20160412072653) do
 
   create_table "activities", force: :cascade do |t|
     t.text     "action"
@@ -49,7 +49,10 @@ ActiveRecord::Schema.define(version: 20160410155443) do
     t.datetime "end_date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "users_id"
   end
+
+  add_index "courses", ["users_id"], name: "index_courses_on_users_id"
 
   create_table "subjects", force: :cascade do |t|
     t.string   "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,60 +5,49 @@ User.create! name: "Vinh2", email: "vinh2@gmail.com", admin: true,
 User.create! name: "user", email: "user@user.com", admin: true, role: 0,
   password: "foobar", password_confirmation: "foobar"
 
-5.times do |n|
-  Course.create! name: "Course-#{n+1}", description: "Traning course"
+10.times do |n|
+  User.create! name: "user#{n+1}", email: "user#{n+1}@user.com", role: 0,
+  password: "foobar", password_confirmation: "foobar"
 end
+
+Course.create! name: "Course-1", description: "Traning course"
 
 5.times do |n|
   Subject.create! name: "Subject-#{n+1}", description: "Traning subject"
 end
 
 5.times do |n|
-  Task.create! name: "Task-#{n+1}"
+  Task.create! name: "Task-#{n+1}", subject_id: n+1
 end
 
 5.times do |n|
   CourseSubject.create! course_id: 1, subject_id: n+1
 end
-5.times do |n|
-  CourseSubject.create! course_id: 2, subject_id: n+1
-end
-5.times do |n|
-  CourseSubject.create! course_id: 3, subject_id: n+1
-end
-5.times do |n|
-  CourseSubject.create! course_id: 4, subject_id: n+1
-end
-5.times do |n|
-  CourseSubject.create! course_id: 5, subject_id: n+1
-end
 
 5.times do |n|
-  UserSubject.create! user_id: 1, course_subject_id: n+1, status: "Started"
-end
-5.times do |n|
-  UserSubject.create! user_id: 2, course_subject_id: n+1, status: "Started"
+  UserSubject.create! user_id: 4, course_subject_id: n+1, status: "Started"
 end
 
-3.times do |n|
-  UserTask.create! user_subject_id: 1, course_subject_task_id: n+1
-end
-3.times do |n|
-  UserTask.create! user_subject_id: 2, course_subject_task_id: n+1
-end
+# 3.times do |n|
+#   UserTask.create! user_subject_id: 1, course_subject_task_id: n+1
+# end
 
-5.times do |n|
-  CourseSubjectTask.create! course_subject_id: 1, task_id: n+1
-end
-5.times do |n|
-  CourseSubjectTask.create! course_subject_id: 2, task_id: n+1
-end
+# 3.times do |n|
+#   UserTask.create! user_subject_id: 2, course_subject_task_id: n+1
+# end
 
-5.times do |n|
-  10.times do |m|
-    Task.create! subject_id: n+1,
-      name: "Task-#{m+1}",
-      content: Faker::Lorem.paragraphs(rand(2..8)),
-      modify: false
-  end
-end
+# 5.times do |n|
+#   CourseSubjectTask.create! course_subject_id: 1, task_id: n+1
+# end
+# 5.times do |n|
+#   CourseSubjectTask.create! course_subject_id: 2, task_id: n+1
+# end
+
+# 5.times do |n|
+#   10.times do |m|
+#     Task.create! subject_id: n+1,
+#       name: "Task-#{m+1}",
+#       content: Faker::Lorem.paragraphs(rand(2..8)),
+#       modify: false
+#   end
+# end
