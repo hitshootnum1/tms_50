a6627c3a4150e0e1366fd29eadf2c0803fee1a47
HUSTToanLH <laihuutoan@gmail.com>
2016-04-14 15:48:30 +0700

    course members

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,5 +4,9 @@ class CoursesController < ApplicationController
 
   def show
     @activities = PublicActivity::Activity.all
+    @members = []
+    @course.user_courses.each do |user_course|
+      @members.push user_course.user if user_course.user
+    end
   end
 end
