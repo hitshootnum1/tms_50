0bb99dc6eced6f5270982fa08c5185d00f8b4e56
Tran Tien Duc <hitshootnum1@gmail.com>
2016-04-19 13:25:12 +0700

    Finish install rspec, simplecov, user-models

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -91,4 +91,5 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each { |f| require f }
 end
